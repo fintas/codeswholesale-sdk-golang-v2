43a9467f286cfd4534fb3d5e6ed95076d8c605a1
David Feldhofer <david.feldhofer@fintas.at>
2018-12-13 12:46:10 +0100

    apiGET now with variadic parameter

--- sdk.go
+++ sdk.go
@@ -1,9 +1,12 @@
 package codeswholesalesdkgolangv2
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"path"
 )
 
 const liveEndpointURL = "https://api.codeswholesale.com"
@@ -28,7 +31,7 @@ func NewSDK(clientID string, clientSecret string, clientSignature string, live b
 	}
 }
 
-func (sdk *SDK) apiGET(path string) ([]byte, error) {
+func (sdk *SDK) apiGET(paths ...string) ([]byte, error) {
 	// Is authorized
 	if !sdk.isAuthorized() {
 		err := sdk.authorize()
@@ -38,16 +41,18 @@ func (sdk *SDK) apiGET(path string) ([]byte, error) {
 	}
 
 	// Endpoint setup.
-	var endpoint string
+	var base *url.URL
+	u, _ := url.Parse(path.Join(paths...))
 	if sdk.live {
-		endpoint = fmt.Sprintf("%s%s", liveEndpointURL, path)
+		base, _ = url.Parse(liveEndpointURL)
 	} else {
-		endpoint = fmt.Sprintf("%s%s", sandboxEndpointURL, path)
+		base, _ = url.Parse(sandboxEndpointURL)
 	}
+	endpoint := base.ResolveReference(u)
 
 	// Build the request.
 	client := &http.Client{}
-	r, err := http.NewRequest("GET", endpoint, nil)
+	r, err := http.NewRequest("GET", endpoint.String(), nil)
 	if err != nil {
 		return nil, err
 	}
@@ -58,6 +63,9 @@ func (sdk *SDK) apiGET(path string) ([]byte, error) {
 	resp, err := client.Do(r)
 	if err != nil {
 		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
 	}
 
 	// Get the request body.
